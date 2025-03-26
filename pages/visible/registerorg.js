654e5f31c8baddbd0fb11f1b08a65dcaab4124f4
Miro Gohacki <mirogohacki@gmail.com>
2025-03-26 15:49:31 -0400

    updating register org page

--- pages/visible/registerorg.js
+++ pages/visible/registerorg.js
@@ -21,7 +21,7 @@ const RegisterOrganization = () => {
     }
   }, [user, loading, router]);
 
-  // State to hold form data
+  // State to hold form data and image preview URL
   const [orgData, setOrgData] = useState({
     orgName: '',
     orgWebsite: '',
@@ -32,6 +32,7 @@ const RegisterOrganization = () => {
     zipCode: '',
     orgPhoto: null,
   });
+  const [previewUrl, setPreviewUrl] = useState(null);
 
   // Track the current step of the wizard
   const [currentStep, setCurrentStep] = useState(1);
@@ -41,10 +42,12 @@ const RegisterOrganization = () => {
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     if (name === 'orgPhoto') {
+      const file = files[0];
       setOrgData((prev) => ({
         ...prev,
-        orgPhoto: files[0],
+        orgPhoto: file,
       }));
+      setPreviewUrl(URL.createObjectURL(file));
     } else {
       setOrgData((prev) => ({
         ...prev,
@@ -118,37 +121,42 @@ const RegisterOrganization = () => {
       case 1:
         return (
           <div className="space-y-4">
+            <h2 className="text-2xl font-bold mb-4">First, Let&apos;s Set Up Your Organization</h2>
+            <h3 className="text-lg font-semibold">Organization Name</h3>
             <input
               type="text"
               name="orgName"
               value={orgData.orgName}
               onChange={handleChange}
-              placeholder="Organization Name"
+              placeholder="e.g. Salvation Army"
               required
-              className="border px-3 py-2 w-full"
+              className="border px-3 py-2 w-full rounded"
             />
+            <h3 className="text-lg font-semibold">Organization Website</h3>
             <input
               type="text"
               name="orgWebsite"
               value={orgData.orgWebsite}
               onChange={handleChange}
-              placeholder="Organization Website"
+              placeholder="e.g. salvationarmyusa.org"
               required
-              className="border px-3 py-2 w-full"
+              className="border px-3 py-2 w-full rounded"
             />
+            <h3 className="text-lg font-semibold">Description</h3>
             <textarea
               name="orgDescription"
               value={orgData.orgDescription}
               onChange={handleChange}
-              placeholder="Organization Description"
+              placeholder="Donate today to help a neighbor in need find comprehensive aid through The Salvation Army's support programs."
               required
-              className="border px-3 py-2 w-full"
+              className="border px-3 py-2 w-full rounded"
             />
           </div>
         );
       case 2:
         return (
-          <div className="space-y-4">
+          <div className="space-y-8">
+            <h2 className="text-2xl font-bold mb-4">Where should we send donations?</h2>
             <input
               type="text"
               name="streetAddress"
@@ -156,7 +164,7 @@ const RegisterOrganization = () => {
               onChange={handleChange}
               placeholder="Street Address"
               required
-              className="border px-3 py-2 w-full"
+              className="border px-3 py-2 w-full rounded"
             />
             <div className="flex space-x-4">
               <input
@@ -166,7 +174,7 @@ const RegisterOrganization = () => {
                 onChange={handleChange}
                 placeholder="City"
                 required
-                className="border px-3 py-2 w-full"
+                className="border px-3 py-2 w-full rounded"
               />
               <input
                 type="text"
@@ -175,7 +183,7 @@ const RegisterOrganization = () => {
                 onChange={handleChange}
                 placeholder="State"
                 required
-                className="border px-3 py-2 w-full"
+                className="border px-3 py-2 w-full rounded"
               />
             </div>
             <input
@@ -185,21 +193,42 @@ const RegisterOrganization = () => {
               onChange={handleChange}
               placeholder="Zip Code"
               required
-              className="border px-3 py-2 w-full"
+              className="border px-3 py-2 w-full rounded"
             />
           </div>
         );
       case 3:
         return (
           <div className="space-y-4">
+            <label className="block text-lg font-semibold">Pick a Photo For Your Organization</label>
+            {previewUrl ? (
+              <div className="mb-4">
+                <img
+                  src={previewUrl}
+                  alt="Preview"
+                  className="max-w-full h-auto rounded shadow-md"
+                />
+              </div>
+            ) : (
+              <div className="mb-4 border-2 border-dashed border-gray-300 p-4 rounded text-center">
+                <p>No image selected. Please upload your organization&apos;s photo.</p>
+              </div>
+            )}
             <input
               type="file"
               name="orgPhoto"
               onChange={handleChange}
               accept="image/*"
               required
-              className="border px-3 py-2 w-full"
+              className="hidden"
+              id="orgPhotoInput"
             />
+            <label
+              htmlFor="orgPhotoInput"
+              className="cursor-pointer inline-block bg-ggreen text-white px-6 py-2 rounded-full hover:bg-teal-800"
+            >
+              Choose Photo
+            </label>
           </div>
         );
       default:
@@ -211,45 +240,42 @@ const RegisterOrganization = () => {
   const progressPercentage = (currentStep / totalSteps) * 100;
 
   return (
-    <div className="container mx-auto px-4 pt-40 pb-32">
-      <h2 className="text-2xl font-bold mb-4">Register Your Organization</h2>
-      {renderStep()}
-
+    <div className="container mx-auto mt-20 px-4 min-h-screen flex flex-col pt-20 pb-64">
+      {/* Main Content */}
+      <div className="flex-1">
+        {renderStep()}
+      </div>
       
-
-      {/* Navigation Buttons */}
-      <div className="flex justify-between mt-6">
-      <div className="w-full mr-8">
-      <p className="text-center mt-2">
-          Step {currentStep} of {totalSteps}
-        </p>
-        <div className="w-full bg-gray-200 rounded-full">
-        <div
-          style={{ width: `${Math.round(progressPercentage)}%` }}
-          className="bg-ggreen text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full"
-        >
-          {Math.round(progressPercentage)}%
+      {/* Footer / Navigation Buttons */}
+      <div className="mt-12 flex">
+        <div className="w-full mr-8">
+          <div className="w-full bg-gray-200 rounded-full">
+            <div
+              style={{ width: `${Math.round(progressPercentage)}%` }}
+              className="bg-ggreen text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full"
+            />
+          </div>
+          <p className="text-center mt-2">
+            Step {currentStep} of {totalSteps}
+          </p>
         </div>
-      </div>
-
-
-      </div>
-
-        {currentStep > 1 && (
+  
+        <div className="flex justify-between">
+          {currentStep > 1 && (
+            <button
+              onClick={prevStep}
+              className="border-2 border-ggreen text-ggreen px-12 py-3 mr-4 rounded-full hover:bg-ggreen hover:text-white"
+            >
+              Back
+            </button>
+          )}
           <button
-            onClick={prevStep}
-            className="border-2 border-ggreen text-ggreen px-12 py-3 rounded-full hover:bg-ggreen hover:text-white ml-auto"
+            onClick={nextStep}
+            className="border-2 border-ggreen bg-ggreen text-white px-12 py-3 rounded-full hover:bg-teal-800"
           >
-            Back
+            {currentStep === totalSteps ? 'Submit' : 'Next'}
           </button>
-        )}
-        <button
-          onClick={nextStep}
-          className="bg-ggreen text-white px-12 py-3 rounded-full hover:bg-teal-800 ml-12"
-        >
-          {currentStep === totalSteps ? 'Submit' : 'Next'}
-        </button>
-
+        </div>
       </div>
     </div>
   );
