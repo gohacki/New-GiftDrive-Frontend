5c56813dc5048b8bfc0049ed4cb703f646b14469
Miro Gohacki <mirogohacki@gmail.com>
2025-05-16 23:34:22 -0400

    trying to get favicon to work

--- pages/_document.js
+++ pages/_document.js
@@ -8,27 +8,36 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="theme-color" content="#000000" /> {/* Or your actual theme color */}
+          <meta name="theme-color" content="#11393B" /> {/* Updated to your ggreen color */}
 
-          {/* Favicons and Manifest */}
-          <link rel="icon" type="image/png" href="/img/brand/favicon-96x96.png" sizes="96x96" />
-          <link rel="icon" type="image/svg+xml" href="/img/brand/favicon.svg" />
-          <link rel="shortcut icon" href="/img/brand/favicon.ico" />
+          {/* Favicons & App Icons */}
+          {/* For modern browsers - SVG is preferred */}
+          <link rel="icon" href="/img/brand/favicon.svg" type="image/svg+xml" />
+          {/* Fallback ICO for older browsers/some crawlers (place in public/favicon.ico) */}
+          <link rel="shortcut icon" href="/favicon.ico" />
+          {/* PNG favicons for different resolutions */}
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/brand/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/brand/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/img/brand/favicon-96x96.png" />
+          {/* Apple Touch Icon */}
           <link rel="apple-touch-icon" sizes="180x180" href="/img/brand/apple-touch-icon.png" />
-          <meta name="apple-mobile-web-app-title" content="GiftDrive" /> {/* Updated title */}
+          {/* Web App Manifest */}
           <link rel="manifest" href="/img/brand/site.webmanifest" />
+          <meta name="apple-mobile-web-app-title" content="GiftDrive" />
+          <meta name="application-name" content="GiftDrive" /> {/* Good to add for completeness */}
+          <meta name="msapplication-TileColor" content="#11393B" /> {/* For Windows Tiles */}
+          {/* Optional: <meta name="msapplication-TileImage" content="/img/brand/mstile-144x144.png" /> */}
+
 
           {/* Google Fonts - Inter */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          {/* Ensure you request all necessary weights for Inter */}
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
             rel="stylesheet"
           />
-          {/* Removed Montserrat as it's not the primary font anymore */}
         </Head>
-        <body className="text-blueGray-700 antialiased"> {/* This class will now use Inter due to body style changes */}
+        <body className="text-slate-700 antialiased"> {/* Changed blueGray to slate for consistency */}
           <div id="page-transition"></div>
           <Main />
           <NextScript />
